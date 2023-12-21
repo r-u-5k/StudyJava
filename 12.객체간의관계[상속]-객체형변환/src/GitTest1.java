b17ee050364e797af6db5f4be968ceb52ef0bd73
장유진 <yj999900@gmail.com>
2023-12-21 18:19:47 +0900

    .

--- 12.객체간의관계[상속]-객체형변환/src/GitTest1.java
+++ 12.객체간의관계[상속]-객체형변환/src/GitTest1.java
@@ -2,4 +2,5 @@
 public class GitTest1 {
 	int member1;
 	int member2;
+	int member3;
 }
--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,46 +7,22 @@ public class Main {
 
 		Scanner scanner = new Scanner(System.in);
 		
-		/* n
-		 * 1~1 -> 1개 / 1
-		 * 2~7 -> 2개 / 6
-		 * 8~19 -> 3개 / 12
-		 * 20~37 -> 4개 / 18
-		 */
-		
 		int n = scanner.nextInt();
-		int r = 0;
+		double[] score = new double[n];
+		for (int i = 0; i < n; i++) {
+			score[i] = scanner.nextInt();
+		}
+		Arrays.sort(score);
+		double max = score[n - 1];
+		double sum = 0;
 		
 		for (int i = 0; i < n; i++) {
-			
+			score[i] = score[i] * 100 / max;
+			sum += score[i];
 		}
 		
-		
-		if (n >= 0 && n < 2) {
-			r = 1;
-		}
-		if (n >= 2 && n < 8) {
-			r = 2;
-		}
-		if (n >= 8 && n < 20) {
-			r = 3;
-		}
+		System.out.println(sum / n);
 		
 		scanner.close();
 	}
 }
-
-/*
-int n = scanner.nextInt();
-int num = 0;
-for (int i = 0; i < n; i++) {
-	if (i + i / 100 + (i % 100) / 10 + i % 10 == n) { // 3자리
-		num = i;
-		break;
-	} else {
-		num = 0;
-	}
-}
-if i + (i의 각 자릿수들) = n
-
-*/
