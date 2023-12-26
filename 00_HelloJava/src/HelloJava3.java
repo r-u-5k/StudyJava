0d890d9a4c519f10fce027a6996da9d6d49ad0d5
장유진 <yj999900@gmail.com>
2023-12-26 16:22:22 +0900

    .

--- 00_HelloJava/src/HelloJava3.java
+++ 00_HelloJava/src/HelloJava3.java
@@ -2,9 +2,9 @@
 public class HelloJava2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java2");
+		System.out.println("Hello Java3");
 		// content assist: ctrl+space 
-		System.out.println("안녕 자바2");
+		System.out.println("안녕 자바3");
 		
 	}
 
