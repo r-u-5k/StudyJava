b0ee71f692dff708838c22ec45d5f8ae377507ee
장유진 <yj999900@gmail.com>
2023-12-27 10:13:23 +0900

    .

--- 06.제한자[Static]/doc/type-search-index.js
+++ 06.제한자[Static]/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Car"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Calculator"},{"p":"<Unnamed>","l":"CalculatorMain"},{"p":"<Unnamed>","l":"Car"},{"p":"<Unnamed>","l":"CarFactoryStaticMain"},{"p":"<Unnamed>","l":"StaticMain"}];updateSearchResults();
--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -5,35 +5,21 @@ public class Main {
 
 		Scanner scanner = new Scanner(System.in);
 		
-		String s = scanner.next();
-		char[] c = new char[s.length()];
-		int time = 0;
+		int n = scanner.nextInt(); // nCk = n!/((n-k)!*k!)
+		int k = scanner.nextInt();
+		int a = 1, b = 1, c = 1;
 		
-		for (int i = 0; i < c.length; i++) {
-			c[i] = s.charAt(i);
+		for (int i = 1; i <= n; i++) {
+			a *= i;
+		}
+		for (int i = 1; i <= k; i++) {
+			b *= i;
+		}
+		for (int i = 1; i <= n - k; i++) {
+			c *= i;
 		}
 		
-		for (int i = 0; i < c.length; i++) {
-			if (c[i] >= 'A' && c[i] <= 'C') {
-				time += 3;
-			} else if (c[i] >= 'D' && c[i] <= 'F') {
-				time += 4;
-			} else if (c[i] >= 'G' && c[i] <= 'I') {
-				time += 5;
-			} else if (c[i] >= 'J' && c[i] <= 'L') {
-				time += 6;
-			} else if (c[i] >= 'M' && c[i] <= 'O') {
-				time += 7;
-			} else if (c[i] >= 'P' && c[i] <= 'S') {
-				time += 8;
-			} else if (c[i] >= 'T' && c[i] <= 'V') {
-				time += 9;
-			} else if (c[i] >= 'W' && c[i] <= 'Z') {
-				time += 10;
-			}
-		}
-		
-		System.out.println(time);
+		System.out.println(a / (b * c));
 		
 		scanner.close();
 	}
