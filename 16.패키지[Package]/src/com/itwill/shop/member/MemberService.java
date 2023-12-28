a1864459621c381636ebb7b928b69afdb53f05c4
장유진 <yj999900@gmail.com>
2023-12-28 11:03:19 +0900

    .

--- 16.패키지[Package]/src/com/itwill/shop/member/MemberService.java
+++ 16.패키지[Package]/src/com/itwill/shop/member/MemberService.java
@@ -1,5 +1,6 @@
 package com.itwill.shop.member;
 
 public class MemberService {
-
+	private com.itwill.shop.member.Member member1;
+	private Member member2; // 같은 패키지 안에 있으면 import 생략
 }
--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -4,58 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		String[] subject = new String[20];
-		String[] grade = new String[20];
-		double[] score = new double[20];
-		double[] credit = new double[20];
-		double[] mulOfScoreCredit = new double[20];
-		double sumOfCredit = 0;
-		double sumAll = 0;
 		
-		for (int i = 0; i < 20; i++) {
-			subject[i] = scanner.next();
-			credit[i] = scanner.nextDouble();
-			grade[i] = scanner.next();
-		}
+		int n = scanner.nextInt();
+		int[] num = new int[n];
+		for (int i = 0; i < n; i++) {
 			
-		for (int i = 0; i < 20; i++) { // 과목평점
-			if (grade[i].equals("A+")) {
-				score[i] = 4.5;
-			} else if (grade[i].equals("A0")) {
-				score[i] = 4.0;
-			} else if (grade[i].equals("B+")) {
-				score[i] = 3.5;
-			} else if (grade[i].equals("B0")) {
-				score[i] = 3.0;
-			} else if (grade[i].equals("C+")) {
-				score[i] = 2.5;
-			} else if (grade[i].equals("C0")) {
-				score[i] = 2.0;
-			} else if (grade[i].equals("D+")) {
-				score[i] = 1.5;
-			} else if (grade[i].equals("D0")) {
-				score[i] = 1.0;
-			} else if (grade[i].equals("F")) {
-				score[i] = 0.0;
-			} else if (grade[i].equals("P")) {
-				score[i] = 0.0;
-			}
 		}
-		
-		for (int i = 0; i < 20; i++) {
-			if (!grade[i].equals("P")) {
-				sumOfCredit += credit[i];
-			}
-		}
-		
-		for (int i = 0; i < 20; i++) {
-			if (!grade[i].equals("P")) {
-				mulOfScoreCredit[i] = score[i] * credit[i];
-				sumAll += mulOfScoreCredit[i];
-			}
-		}
-		
-		System.out.println(sumAll / sumOfCredit);
 		
 		scanner.close();
 	}
