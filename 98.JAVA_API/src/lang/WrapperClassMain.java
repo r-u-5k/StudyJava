e41d352479160b301b35de22ceae8846e65ae127
장유진 <yj999900@gmail.com>
2024-01-02 14:26:20 +0900

    .

--- 98.JAVA_API/src/lang/WrapperClassMain.java
+++ 98.JAVA_API/src/lang/WrapperClassMain.java
@@ -38,6 +38,68 @@ public class WrapperClassMain {
 		Double wd2 = 1.1;
 		Boolean wb2 = true;
 		
+		/*
+		 * Auto Unboxing [Wrapper 객체 -> 기본형]
+		 */
+		char c2 = wc2;
+		int i2 = wi2;
+		double d2 = wd2;
+		boolean b2 = wb2;
+		
+		System.out.println("*** AUTO BOXING ***");
+		int a = 89;
+		int b = 45;
+		int c = 78;
+		int d = 90;
+		int e = 99;
+		Integer ia = new Integer(a);
+		Integer ib = new Integer(b);
+		Integer ic = new Integer(c);
+		Integer id = new Integer(d);
+		Integer ie = new Integer(e);
+		
+		System.out.println("*** AUTO CASTING (자동형변환) ***");
+		Object oa = ia;
+		Object ob = ib;
+		Object oc = ic;
+		Object od = id;
+		Object oe = ie;
+		
+		System.out.println("*** AUTO BOXING, AUTO CASTING ***");
+		Object ooa = 89;
+		Object oob = 45;
+		Object ooc = 78;
+		Object ood = 90;
+		Object ooe = 99;
+		
+		System.out.println("*** Object[] ***");
+		Object[] scoreArray = new Object[5];
+		scoreArray[0] = 89;
+		scoreArray[1] = 45;
+		scoreArray[2] = 78;
+		scoreArray[3] = 90;
+		scoreArray[4] = 99;
+		
+		for (int i = 0; i < scoreArray.length; i++) {
+			/*
+			Integer tempScoreInteger = (Integer) scoreArray[i];
+			int tempScore = tempScoreInteger.intValue();
+			*/
+			int tempScore = (Integer) scoreArray[i];
+			System.out.print(tempScore + " ");
+		}
+		System.out.println();
+		
+		/*
+		 * Wrapper 객체의 equals 메서드
+		 */
+		System.out.println("*** equals ***");
+		Integer wi11 = new Integer(123);
+		Integer wi22 = new Integer(123);
+		System.out.println("주소 비교(==): " + (wi11 == wi22));
+		System.out.println("equals(재정의 -> 기본형 멤버 필드 값 비교): " + wi11.equals(wi22));
+		
+		
 		
 		
 	}
