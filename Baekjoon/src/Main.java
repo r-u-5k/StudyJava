e2df295764e978daeb4af727d74c3f7609b7b572
장유진 <yj999900@gmail.com>
2024-01-26 18:18:09 +0900

    .

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.Iterator;
@@ -12,15 +13,25 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		Deque<Integer> commentDeque = new ArrayDeque<Integer>();
+		List<Integer> list = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
-			commentDeque.add(Integer.parseInt(br.readLine()));
+			list.add(Integer.parseInt(br.readLine()));
 		}
-		int totPeople = commentDeque.size();
+		int totPeople = list.size();
 		int noReflectPeople = (int) Math.round(totPeople * 0.15);
 		
+		Collections.sort(list);
 		for (int i = 0; i < noReflectPeople; i++) {
-			
+			list.remove(i);
+			list.remove(list.size() - 1 - i);
 		}
+		
+		int sum = 0;
+		for (Integer integer : list) {
+			sum += integer;
+		}
+		
+		int answer = (int) Math.round(sum * 1.0 / (totPeople - 2 * noReflectPeople));
+		System.out.println(answer);
 	}
 }
