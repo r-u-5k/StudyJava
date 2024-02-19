3c4524da1c8b644ba23055c7d6d8711d9a1f4749
장유진 <yj999900@gmail.com>
2024-02-19 09:50:00 +0900

    .

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			String s = sc.nextLine();
+			StringTokenizer st = new StringTokenizer(s, " ");
+			List<String> list = new ArrayList<>();
+			while (!st.hasMoreTokens()) {
+				list.add(st.nextToken());
+			}
+			for (int j = st.nextToken().length() - 1; j >= 0 ; j--) {
+				System.out.println(st.nextToken().charAt(j));
+			}
+			
+		}
+
 	}
 }
